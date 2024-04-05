4e43416db7369bee697048b2a07683ad38ab60a5
Nathen Priyonggo <nagapri16@gmail.com>
2024-04-04 23:38:49 -0500

    message class started

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -2,4 +2,11 @@ import java.io.Serializable;
 
 public class Message implements Serializable {
     static final long serialVersionUID = 42L;
+    private String chat;
+    private String username;
+
+    public Message(String username, String chat) {
+        this.username = username;
+        this.chat = chat;
+    }
 }
